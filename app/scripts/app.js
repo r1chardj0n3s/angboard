724bfdefd56429179a18f14f733403068355704a
Richard Jones <r1chardj0n3s@gmail.com>
2014-10-13 15:50:06 +1100

    remove unused angular-sanitize

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var app = angular.module('angboardApp', ['ui.bootstrap', 'ngRoute', 'smart-table',
-    'LocalStorageModule', 'ngSanitize']);
+    'LocalStorageModule']);
 
   app.config(function ($routeProvider) {
     // set up the default route
